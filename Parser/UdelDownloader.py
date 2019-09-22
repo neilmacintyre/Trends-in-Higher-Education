15115094131fea875a54233a0b920cbe4a66c5d6
Neil Macintyre <neil.macintyre@neilmacintyre.com>
2019-09-22 19:32:22 +0100

    Initialize github pages branch

--- Parser/UdelDownloader.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-res = requests.get('https://www.udel.edu/apply/career-outcomes/jsondata/jsondata.json')
-with open('../Data/Raw/Udel/data.json', 'w') as file:
-    file.write(res.text)
